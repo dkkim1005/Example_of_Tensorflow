7bd6878b8a1b6a961eb458aff847e1e802da6221
Dongkyu Kim <dkkim1005@gist.ac.kr>
2017-02-09 20:07:14 +0900

    Add files via upload

--- MLP/mlp_class.py
+++ MLP/mlp_class.py
@@ -2,51 +2,7 @@
 import numpy as np
 import tensorflow as tf
 
-
-def inLayerGenerator(outLayer, outNumDim, inNumDim, dtype='float32'):
-    # Generates next layer with normalized signal.
-
-    """
-    neuron  out   xW     in    neuron
-       0    ---    X    ---     0
-       0    ---    X    ---     0
-       ..          X    ---     ..
-       0    ---    X    ---     0
-       0    ---    X    ---     0
-                  +b
-    """
-    W = tf.Variable(tf.random_uniform([outNumDim,inNumDim]),dtype=dtype)
-    b = tf.Variable(tf.random_uniform([inNumDim]),dtype=dtype)
-
-    inLayer = tf.matmul(tf.cast(outLayer,dtype),W) + b
-
-    return inLayer
-
-
-def multiLayerGenerator(inputLayer, numNodeList, active = tf.sigmoid, prob_dropout = 1):
-    # The # of hidden layer should be larger than 1.
-    assert(len(numNodeList) >= 3)    
-
-    _outLayer = inputLayer
-    size = len(numNodeList)
-
-    # Connect hidden layer
-    for i in range(size-2):
-        outNumDim = numNodeList[i]
-        inNumDim = numNodeList[i+1]
-        _inLayer = inLayerGenerator(_outLayer,outNumDim,inNumDim)
-        _outLayer = active(_inLayer)
-        _outLayer = tf.nn.dropout(_outLayer,prob_dropout)
-
-    finOutNumDim = numNodeList[-2];
-    finInNumDim = numNodeList[-1];
-
-    totLayer = inLayerGenerator(_outLayer, finOutNumDim, finInNumDim)
-
-    return totLayer
-
-
-class dataInsertion:
+class ANN_MLP_Base:
     def __init__(self):
         self.inData = []
         self.outData = []
@@ -69,46 +25,116 @@ class dataInsertion:
             for j,datum in enumerate(outData[i]):
                 self.outData[self.numTail].append(datum)
 
-        self.inData = np.array(self.inData)
-        self.outData = np.array(self.outData)
+
+    def run(self, sess, numIter, batchRatio=1.):
+        assert(isinstance(sess,tf.Session))
+        
+        inData = np.array(self.inData)
+        outData = np.array(self.outData)
+
+        totSize = self.numTail + 1
+
+        cutSize = int(totSize * batchRatio)
+
+        for niter in xrange(numIter):
+            arr = range(totSize-1)
+            arr = np.random.permutation(arr)
+            arr = arr[:cutSize]
+            sess.run(self.train,feed_dict={self.inLayer : inData[arr], self.outLayer : outData[arr]})
+            if(niter % 10 == 0):
+                print "iter:",niter,"cost :",\
+                      sess.run(self.cost,feed_dict={self.inLayer : inData, self.outLayer : outData})
+
+        print "cost :",sess.run(self.cost,feed_dict={self.inLayer : inData, self.outLayer : outData})
 
 
-class ANN_Regression(dataInsertion):
+    def predict(self, sess, x):
+        return sess.run(self.totLayer,feed_dict={self.inLayer : x})
+
+
+    def save(self, sess, nameFile, ver=1):
+        assert(isinstance(sess,tf.Session))
+
+        tf.train.Saver().save(sess,nameFile,ver)
+
+
+    def inLayerGenerator__(self, outLayer, outNumDim, inNumDim, dtype='float32'):
+        # Generates next layer with normalized signal.
+
+        """
+        neuron  out   xW     in    neuron
+           0    ---    X    ---     0
+           0    ---    X    ---     0
+           ..          X    ---     ..
+           0    ---    X    ---     0
+           0    ---    X    ---     0
+                      +b
+        """
+        W = tf.Variable(tf.random_uniform([outNumDim,inNumDim]),dtype=dtype)
+        b = tf.Variable(tf.random_uniform([inNumDim]),dtype=dtype)
+
+        inLayer = tf.matmul(tf.cast(outLayer,dtype),W) + b
+
+        return inLayer
+
+
+    def multiLayerGenerator__(self, inputLayer, numNodeList, active = tf.sigmoid, prob_dropout = 1):
+        # The # of hidden layer should be larger than 1.
+        assert(len(numNodeList) >= 3)    
+
+        _outLayer = inputLayer
+        size = len(numNodeList)
+
+        # Connect hidden layer
+        for i in range(size-2):
+            outNumDim = numNodeList[i]
+            inNumDim = numNodeList[i+1]
+            _inLayer = self.inLayerGenerator__(_outLayer,outNumDim,inNumDim)
+            _outLayer = active(_inLayer)
+            _outLayer = tf.nn.dropout(_outLayer,prob_dropout)
+
+        finOutNumDim = numNodeList[-2];
+        finInNumDim = numNodeList[-1];
+
+        totLayer = self.inLayerGenerator__(_outLayer, finOutNumDim, finInNumDim)
+
+        return totLayer
+
+
+
+
+class ANN_MLP_Regression(ANN_MLP_Base):
     def __init__(self, learning_rate, numNodeList, active = tf.nn.softmax, prob_dropout = 1.0, dtype='float32'):
-        dataInsertion.__init__(self)
+        ANN_MLP_Base.__init__(self)
         inNumDim = numNodeList[0]
         outNumDim = numNodeList[-1]
 
         self.inLayer = tf.placeholder(dtype,[None,inNumDim])
         self.outLayer = tf.placeholder(dtype,[None,outNumDim])
-        self.totLayer = multiLayerGenerator(self.inLayer, numNodeList, active, prob_dropout)
+        self.totLayer = self.multiLayerGenerator__(self.inLayer, numNodeList, active, prob_dropout)
 
         self.cost = tf.reduce_mean(tf.reduce_sum(tf.square(self.outLayer - self.totLayer),reduction_indices=[1]))
 
         self.train = tf.train.AdamOptimizer(learning_rate).minimize(self.cost)
 
 
-    def run(self, sess, numIter, batchSize=1.):
-        assert(isinstance(sess,tf.Session))
 
-        totSize = self.numTail + 1
+class ANN_Classification(ANN_MLP_Base):
+    def __init__(self, learning_rate, numNodeList, active = tf.nn.softmax, prob_dropout = 1.0, dtype='float32'):
+        ANN_MLP_Base.__init__(self)
+        inNumDim = numNodeList[0]
+        outNumDim = numNodeList[-1]
 
-        cutSize = int(totSize * batchSize)
+        self.inLayer = tf.placeholder(dtype,[None,inNumDim])
+        self.outLayer = tf.placeholder(dtype,[None,outNumDim])
+        self.totLayer = self.multiLayerGenerator__(self.inLayer, numNodeList, active, prob_dropout)
+        self.totLayer = active(self.totLayer)
 
-        for niter in xrange(numIter):
-            arr = range(totSize-1)
-            arr = np.random.permutation(arr)
-            arr = arr[:cutSize]
-            sess.run(self.train,feed_dict={self.inLayer : self.inData[arr], self.outLayer : self.outData[arr]})
-            if(niter % 10 == 0):
-                print "iter:",niter,"cost( |y - y'|^2 ):",\
-                      sess.run(self.cost,feed_dict={self.inLayer : self.inData, self.outLayer : self.outData})
+        self.cost = tf.reduce_mean(tf.reduce_sum( -(self.outLayer * tf.log(self.totLayer))  ,reduction_indices=[1]))
 
-        print "cost( |y - y'|^2 ):",sess.run(self.cost,feed_dict={self.inLayer : self.inData, self.outLayer : self.outData})
+        self.train = tf.train.AdamOptimizer(learning_rate).minimize(self.cost)
 
 
-    def predict(self, sess, x):
-        return sess.run(self.totLayer,feed_dict={self.inLayer : x})
 
 
 if __name__ == "__main__":
@@ -128,7 +154,7 @@ if __name__ == "__main__":
     t = t.reshape([numSample,1])
 
     # create ann instance
-    reg = ANN_Regression(learning_rate, numNodeList, active = tf.nn.softmax, prob_dropout = 1.0)
+    reg = ANN_MLP_Regression(learning_rate, numNodeList, active = tf.nn.softmax, prob_dropout = 1.0)
 
     # insert data
     reg.insert_data(t,z)
@@ -137,7 +163,7 @@ if __name__ == "__main__":
     sess.run(tf.initialize_all_variables())
 
     # train ann
-    reg.run(sess,2000,0.8)
+    reg.run(sess,3000,0.8)
 
     # prediction data
     z = reg.predict(sess, t)
